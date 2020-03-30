e621efc14b7f4672aff5cad6552d59041a6b335e
Shad0w7 <ayush.nayak@gmail.com>
2020-03-29 21:31:42 -0700

    Added some files

---
+++ decodeBlinding.py
@@ -0,0 +1,84 @@
+#
+# //     //  //             //  //                      //  //          //
+# //     //                 //                          //              //        //
+# /////  //  //  //////  /////  //  /////// //////      //  //  //////  //////  //////    //////
+# //  // //  //  //  // //  //  //  //   // //  //      //  //  //  //  //  //    //     //
+# /////  //  //  //  //  /////  //  //   // //////      //  //  //////  //  //    // /////
+#                                               //                  //
+#                                           //////              //////
+# created by Shad0w7
+
+# Blinding Decode NOCOMPRESSION v0.0.1a 
+''' [Input]: Colordata 
+    [Output]: String (ASCII)
+    [Description]:  '''
+
+import re
+import sys
+import binascii
+FlexibleInput = False
+
+def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
+    bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
+    return bits.zfill(8 * ((len(bits) + 7) // 8))
+
+def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
+    n = int(bits, 2)
+    return int2bytes(n).decode(encoding, errors)
+
+def int2bytes(i):
+    hex_string = '%x' % i
+    n = len(hex_string)
+    return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
+
+
+
+#Input Stuff
+if FlexibleInput == False: inputstr = "[('#00FFFF', '#808080', '#FF00FF', '#00FF00', '#FF00FF', '#008080', '#FF00FF', '#008080', '#FF00FF', '#FFCC99', '#0000FF', '#FFFFFF', '#00FF00', '#FFFF00', '#FF00FF', '#FFCC99', '#FFFF00', '#0000FF', '#FF00FF', '#008080', '#FF00FF', '#00FFFF')]"
+if FlexibleInput: inputstr = input('InputHexData: ') # Use Custom Input
+
+#Removing All Tuples and Text Stuff and Converting to an easy text 
+hexstr = inputstr + '~~~' #know where the end is
+data = hexstr.split("'")
+rawhex = []
+
+counter = 0
+data[counter]
+
+for z in range(0, len(data)):
+    if '#' in str(data[z]):
+        rawhex.append(data[z])
+    else: continue
+
+
+#8 Bit Color Matrix
+Colors = [
+    ('#FFFFFF', '0000'), #white
+    ('#00FFFF', '0100'), #aqua
+    ('#FF00FF', '0110'), #brightpink
+    ('#FFFF00', '0111'), #yellow
+    ('#C0C0C0', '0011'), #lightgrey
+    ('#FF0000', '0001'), #red
+    ('#00FF00', '0101'), #brightgreen
+    ('#0000FF', '0010'),  #blue
+    ('#808080', '1000'), #darkgre7
+    ('#008080', '1100'), #turquoise
+    ('#FF5005', '1110'), #orange
+    ('#FFCC99', '1111'), #tan
+    ('#993F00', '1011'), #darkorange
+    ('#4C005C', '1001'), #darkpurple
+    ('#94FFB5', '1101'), #lightmintgreen
+    ('#FFA8BB', '1010')] #lightpink
+
+binarylist = []
+for x in range(0, len(rawhex)):
+    for y in range(0, 16):
+        if rawhex[x] == Colors[y][0]:
+            binarylist.append(Colors[y][1])
+
+
+binarystring = ''.join(map(str, binarylist))
+
+originalstring = text_from_bits(binarystring) #!TODO NEED TO ENCODE!!!!
+
+sys.exit(originalstring)
---
+++ encodeBlinding.py
@@ -0,0 +1,95 @@
+#
+# //     //  //             //  //                      //  //          //
+# //     //                 //                          //              //        //
+# /////  //  //  //////  /////  //  /////// //////      //  //  //////  //////  //////    //////
+# //  // //  //  //  // //  //  //  //   // //  //      //  //  //  //  //  //    //     //
+# /////  //  //  //  //  /////  //  //   // //////      //  //  //////  //  //    // /////
+#                                               //                  //
+#                                           //////              //////
+# created by Shad0w7
+
+# Blinding Encode NOCOMPRESSION v0.0.1a 
+''' [Input]: String fixed or with input() 
+    [Output]: ColorMatrix on 10x10 grid to encode and send (with binary 0's afterward)
+    [Description]: This script will take in any string, and output the colormatrix that needs to be sent, if it needs to take up more than one frame, it will send out multiple blocks of different colors'''
+
+import re
+import sys
+import binascii
+import base64
+# -- Bootflags --
+FlexibleInput = False # ONLY IN ALPHA
+# Stack Overflow Stuff
+def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
+    bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
+    return bits.zfill(8 * ((len(bits) + 7) // 8))
+
+def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
+    n = int(bits, 2)
+    return int2bytes(n).decode(encoding, errors)
+
+def int2bytes(i):
+    hex_string = '%x' % i
+    n = len(hex_string)
+    return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
+
+
+# Input
+global inputstr
+if FlexibleInput == False: inputstr = "Hello World"
+if FlexibleInput: inputstr = input('InputStr: ') # Use Custom Input
+
+# Converting String to Binary 
+binaryinput = text_to_bits(inputstr) #! TODO NEED TO CREATE ENCODE/DECODE METHOD!!!!
+
+#8 Bit Color Matrix
+Colors = [
+    ('#FFFFFF', '0000'), #white
+    ('#00FFFF', '0100'), #aqua
+    ('#FF00FF', '0110'), #brightpink
+    ('#FFFF00', '0111'), #yellow
+    ('#C0C0C0', '0011'), #lightgrey
+    ('#FF0000', '0001'), #red
+    ('#00FF00', '0101'), #brightgreen
+    ('#0000FF', '0010'),  #blue
+    ('#808080', '1000'), #darkgre7
+    ('#008080', '1100'), #turquoise
+    ('#FF5005', '1110'), #orange
+    ('#FFCC99', '1111'), #tan
+    ('#993F00', '1011'), #darkorange
+    ('#4C005C', '1001'), #darkpurple
+    ('#94FFB5', '1101'), #lightmintgreen
+    ('#FFA8BB', '1010')] #lightpink
+
+output = [] # output list
+
+# Parsing Color To List
+split = re.findall('.{1,4}', binaryinput)   # may need to use re.finditer() instead of findall because of stringsize
+                                            # has issue where needs exactly 3 on each i think or else will output 000
+for z in range(0, len(split)):
+    block = split[z]
+    for x in range(0,16):
+        if block == Colors[x][1]:
+            output.append(Colors[x][0])     # at this point there is an exaustive list of colors created to represent the different code peices, but it is not rationalized into small peices
+
+cnt = 0
+matrisizedoutput = []
+while cnt < len(output):
+    templist = []
+    if cnt+100 >= len(output): 
+        for a in range(cnt, len(output)):
+            templist.append(output[a])
+            
+    else: 
+        for a in range(cnt, cnt+100):
+            templist.append(output[a])
+            
+    cnt+=100
+
+    matrisizedoutput.append(tuple(templist))
+    templist.clear()
+
+sys.exit(matrisizedoutput)
+    
+
+    
